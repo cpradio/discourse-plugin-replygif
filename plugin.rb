ddcde1ce5e1ae6e684ab44df43d11028ee200df4
cpradio <forums@cpradio.com>
2018-12-01 16:33:52 -0500

    Svg icon (#16)
    * Add composer icon
    
    * Update version number

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,11 @@
 # name: discourse-plugin-replygif
 # about: Add ReplyGIF support
-# version: 1.1.3
+# version: 1.1.4
 # authors: Matthew Wilkin
 # url: https://github.com/cpradio/discourse-plugin-replygif
 
 enabled_site_setting :replygif_enabled
 
 register_asset 'stylesheets/replygif.scss'
+
+register_svg_icon 'play-circle-o' if respond_to?(:register_svg_icon)
